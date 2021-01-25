75eabec10380739d8f95355916b38e8fbfd998b9
m.turaev <tml.murod@mail.ru>
2021-01-25 15:16:13 +0500

    Requesting Multiple Users

--- Requesting Multiple Users.py
+++ Requesting Multiple Users.py
@@ -1,5 +1,6 @@
 import requests
 import json
 r = requests.get('https://randomuser.me/api/?results=50')
-data = r.json()['results'][0]
-print(data)
+data = r.json()['results']
+for i in data:
+    print(i['name']['first'])
